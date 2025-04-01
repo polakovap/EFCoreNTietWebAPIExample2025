72420844964dcf2fe08f6ee6971cd59920e70739
Paul Powell <paul.powell@atu.ie>
2025-04-01 22:50:02 +0100

    Writing up Web API call for showing Product details

--- ProductBlazorApp/Pages/ProductDetail.razor.cs
+++ ProductBlazorApp/Pages/ProductDetail.razor.cs
@@ -1,30 +1,39 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.Toast.Configuration;
+using Blazored.Toast.Services;
+using DataServices;
+using Microsoft.AspNetCore.Components;
 using ProductModel;
 
 namespace ProductBlazorApp.Pages
 {
     public partial class ProductDetail
     {
-        IEnumerable<Product> products;
+        //IEnumerable<Product> products;
         [Parameter]
         public int ID { get; set; }
         public Product Product { get; set; }
 
-        protected override Task OnInitializedAsync()
+        [Inject]
+        //IProductService? productService { get; set; }
+        IHttpClientService? httpService { get; set; }
+        [Inject]
+        public IToastService toastService { get; set; }
+
+        protected override async Task OnInitializedAsync()
         {
-            InitialiseProducts();
-            Product = products.FirstOrDefault(p => p.ID == ID);
-            return base.OnInitializedAsync();
+            if (httpService != null)
+            {
+                toastService.ShowInfo($"Products Loaded", settings => settings.IconType = IconType.None);
+                if (await httpService.GetTokenAsync() != null)
+                    Product = await httpService.getSingle<Product>("api/products/GetProduct/",ID);
+            }
+
+            if (Product == null)
+            {
+                toastService.ShowInfo($"No Product. Check Login", settings => settings.IconType = IconType.None);
+            }
+            await base.OnInitializedAsync();
         }
 
-        private void InitialiseProducts()
-        {
-            products = new List<Product>
-            {
-                new Product{ ID = 1, Description = "Chai", StockOnHand = 12},
-                new Product{ ID = 2, Description = "Syrup", StockOnHand = 10},
-                new Product{ ID = 3, Description = "Cajun Seasoning", StockOnHand = 200},
-            };
-        }
     }
 }
